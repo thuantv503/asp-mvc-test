d912f4cbe1b01cc3f8c1bb313b0d486c8359af2b
Thuantv <thuantv503@gmail.com>
2023-11-23 15:15:54 +0700

    Initial commit

---
+++ TestCoreMVC/TestCoreMVC/Controllers/SinhvienController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestCoreMVC.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TestCoreMVC.Controllers
+{
+    public class SinhvienController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewData["ten"] = "Nguyễn Xuân Tùng";
+            ViewData["maso"] = "015234567";
+
+            return View();
+        }
+        
+        public IActionResult XemDiem()
+        {
+            ViewData["Khoa"] = new Khoa()
+            {
+                MaKhoa = "MK01",
+                TenKhoa = "Hệ thống thông tin"
+            };
+            return View();
+        }
+        public string XemDiem1(string name, double diem = 9, string ID = "1")
+        {
+            return HtmlEncoder.Default.Encode($"ID {ID} Ten {name} Diem trung binh {diem}");
+        }
+        public string XemDiem_temp()
+        {
+            return "Đây là  trang web xem điểm sinh viên";
+        }
+
+    }
+}
---
+++ TestCoreMVC/TestCoreMVC/Models/Khoa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace TestCoreMVC.Models
+{
+    public class Khoa
+    {
+        private string maKhoa;
+        private string tenKhoa;
+
+        public string MaKhoa
+        {
+            get { return maKhoa; }
+            set { maKhoa = value; }
+        }
+        public string TenKhoa
+        {
+            get { return tenKhoa; }
+            set { tenKhoa = value; }
+        }
+        public Khoa()
+        {
+            /*   maKhoa = "";
+               tenKhoa = "";*/
+        }
+        public Khoa(string makhoa, string tenkhoa)
+        {
+            this.maKhoa = makhoa;
+            this.tenKhoa = tenkhoa;
+        }
+
+    }
+}
